25d875c6ff207c3bbf8d7636a1065de7adbc90f3
Arif Ali <meer.aafay@gmail.com>
2025-10-29 09:13:37 -0700

    add new angular cli app - add and import components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Import by Arif
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Home } from './components/home/home';
+import { Contact } from './pages/contact/contact';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // Import by Arif 
+  imports: [RouterOutlet, Header, Footer, Home, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
