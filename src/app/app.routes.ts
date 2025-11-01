d884f342d3a0e2677fdfd36d3fcc4c2dceac7970
Arif Ali <meer.aafay@gmail.com>
2025-11-01 03:06:34 -0700

    add website content and add error-404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
+import { Error404 } from './pages/error404/error404';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: Home,
   },
   {
@@ -17,6 +18,10 @@ export const routes: Routes = [
     component: Contact
   },
 
+  {
+    path: '',
+    component: Error404
+  },
   // todo: not found page route
 
   // {
--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,8 @@ import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
+import { Services } from './pages/services/services';
+import { Error404 } from './pages/error404/error404';
 
 
 @Component({
